700054f973c9363304c441fb423d4b35e1c1f4fa
socio005 <45179042+socio005@users.noreply.github.com>
2018-11-21 22:57:20 +0000

    Pedro

--- Modelo/Biblioteca.java
+++ Modelo/Biblioteca.java
@@ -11,7 +11,14 @@ package GPS.Modelo;
  */
 public interface Biblioteca {
     enum TipoEvento{
-        Reparacao,Mecanica,Menutencoes;
+        Reparacao,Mecanica,Menutencoes,Obrigacoes;
     }
+    
+    static String INSPECAO="Inspecao";
+    static String MUDANCA_OLEO="Mudanca Oleo";
+    static String PAGAMENTO_SEGURO="Pagamento de Seguro";
+    static String MUDANCA_CORREIA="Mudanca de correia";
+    static String PAGAMENTO_IMPOSTO="Pagamento de imposto de circulacao";
+
     
 }
