cd94dc58c58ae90caca35fc77fb01be85c03f25f
Guilherme <guilhermecurado@users.noreply.github.com>
2018-11-22 13:11:38 +0000

    Switch to desktop

--- gpsproject/Controlador.java
+++ gpsproject/Controlador.java
@@ -1,14 +1,24 @@
 package GPS.gpsproject;
 
 import GPS.Modelo.Notification.Notification;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 
-import java.awt.*;
-
 public class Controlador {
+    @FXML
     ListView list;
+    @FXML
     Button addButton;
+
+    @FXML
+    public void initialize() {
+
+    }
 
     public void sendNotification(ActionEvent actionEvent) {
         Notification.sendNotification("Vehicle Companion", "Notification Test");
